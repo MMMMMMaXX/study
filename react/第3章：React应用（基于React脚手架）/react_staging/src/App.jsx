b4e39439e6e0b26976893306d12e18ead58e3113
MMMMMMaXX <2956619600@qq.com>
2021-06-28 18:19:10 +0800

    '210628'

--- react/第3章：React应用（基于React脚手架）/react_staging/src/App.jsx
+++ react/第3章：React应用（基于React脚手架）/react_staging/src/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+
+    getStudentData = () => {
+        axios.get('http://localhost:3000/students').then(
+            // 成功的回调
+            response => { console.log('成功了', response.data); },
+            // 失败的回调
+            error => { console.log('失败了', error); }
+        )
+    }
+
     render() {
         return (
             <div>
-                App...
+                <button onClick={this.getStudentData}>点我获取学生数据</button>
             </div>
         )
     }
