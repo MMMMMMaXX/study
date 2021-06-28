74324dae173ae82794a26af7f39926620d8e3764
MMMMMMaXX <2956619600@qq.com>
2021-06-28 18:23:23 +0800

    '210628'

--- react/第3章：React应用（基于React脚手架）/react_staging/src/App.jsx
+++ react/第3章：React应用（基于React脚手架）/react_staging/src/App.jsx
@@ -12,10 +12,20 @@ export default class App extends Component {
         )
     }
 
+    getCarData = () => {
+        axios.get('http://localhost:3000/cars').then(
+            // 成功的回调
+            response => { console.log('成功了', response.data); },
+            // 失败的回调
+            error => { console.log('失败了', error); }
+        )
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.getStudentData}>点我获取学生数据</button>
+                <button onClick={this.getCarData}>点我获取汽车数据</button>
             </div>
         )
     }
