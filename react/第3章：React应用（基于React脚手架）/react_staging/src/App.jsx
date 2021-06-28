385f026e5324dd90de759ab903ff9a36f52a4957
MMMMMMaXX <2956619600@qq.com>
2021-06-28 11:05:48 +0800

    '210628'

---
+++ react/第3章：React应用（基于React脚手架）/react_staging/src/App.jsx
@@ -0,0 +1,17 @@
+// 创建“外壳”组件App
+// 引入暴露的React、Component
+import React, { Component } from 'react'
+
+// 创建并暴露App组件
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                App...
+            </div>
+        )
+    }
+}
+
+// 暴露App组件
+// export default App
