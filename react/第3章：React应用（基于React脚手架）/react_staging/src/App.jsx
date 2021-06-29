15f483e4bc55284bb68100ddc59125b85f22e957
MMMMMMaXX <2956619600@qq.com>
2021-06-29 19:49:03 +0800

    '210629'

--- react/第3章：React应用（基于React脚手架）/react_staging/src/App.jsx
+++ react/第3章：React应用（基于React脚手架）/react_staging/src/App.jsx
@@ -5,25 +5,12 @@ import './App.css'
 
 export default class App extends Component {
 
-    // 初始化状态，users初始值为数组
-    state = {
-        users: [], //users初始值为数组
-        isFirst: true,//是否为第一次打开页面
-        isLoading: false,//标识是否处于加载中
-        err: ''//存储请求相关错误信息
-    }
-
-    // 更新App的state
-    updateAppState = (stateObj) => {
-        this.setState(stateObj);
-    }
-
     render() {
         return (
             <div>
                 <div className="container">
-                    <Search updateAppState={this.updateAppState} />
-                    <List {...this.state} />
+                    <Search />
+                    <List />
                 </div>
             </div>
         )
--- react/第3章：React应用（基于React脚手架）/react_staging/src/components/Search/index.jsx
+++ react/第3章：React应用（基于React脚手架）/react_staging/src/components/Search/index.jsx
@@ -1,28 +1,35 @@
 import React, { Component } from 'react'
+import PubSub from 'pubsub-js'
 import axios from 'axios'
 
 export default class Search extends Component {
 
     search = () => {
+        console.log('Search组件发布消息了');
         // 获取用户的输入
         // 解构赋值连续写法
         const { keyWordElement: { value: keyWord } } = this;//连续解构赋值
         // console.log(keyWord);
-        // 发送请求前通知App更新状态
-        this.props.updateAppState({ isFirst: false, isLoading: true })
+        // 发送请求前通知List更新状态
+        // this.props.updateAppState({ isFirst: false, isLoading: true })
+        PubSub.publish('mx', { isFirst: false, isLoading: true })
+
         // 发送网络请求
         axios.get(`http://localhost:3000/api1/search/users?q=${keyWord}`).then(
             response => {
-                // 请求成功后通知App更新状态
-                this.props.updateAppState({ isLoading: false, users: response.data.items })
+                // 请求成功后通知List更新状态
+                // this.props.updateAppState({ isLoading: false, users: response.data.items })
+                PubSub.publish('mx',{ isLoading: false, users: response.data.items })
                 console.log('成功了', response.data);
             },
             error => {
-                // 请求失败后通知App更新状态
-                this.props.updateAppState({ isLoading: false, err: error.message })
-                console.log('失败了', error);
+                // 请求失败后通知List更新状态
+                // this.props.updateAppState({ isLoading: false, err: error.message })
+                PubSub.publish('mx',{ isLoading: false, err: error.message })
+                console.log('失败了', error.message);
             }
         )
+        
     }
 
     render() {
