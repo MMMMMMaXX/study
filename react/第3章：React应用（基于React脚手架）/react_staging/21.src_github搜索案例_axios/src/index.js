a896e8d3563ea08074dd2840e0b45fa1cd3c1ff6
MMMMMMaXX <2956619600@qq.com>
2021-06-29 17:32:21 +0800

    '210629'

---
+++ react/第3章：React应用（基于React脚手架）/react_staging/21.src_github搜索案例_axios/src/index.js
@@ -0,0 +1,11 @@
+// 引入React核心库
+import React from 'react'
+// 引入ReactDOM
+import ReactDOM from 'react-dom'
+// 引入App组件
+import App from './App'
+
+// 渲染App到页面
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+// 入口文件
