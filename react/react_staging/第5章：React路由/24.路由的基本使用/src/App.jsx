ca7e4b408e22cf54e695a3a60a917dbabcc805c7
MMMMMMaXX <2956619600@qq.com>
2021-07-02 16:18:19 +0800

    '210702'

---
+++ react/react_staging/第5章：React路由/24.路由的基本使用/src/App.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Link, Route } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <div className="page-header"><h2>React Router Demo</h2></div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+
+                            {/* 原生html中靠<a>跳转到不同的页面 */}
+                            {/* <a className="list-group-item active" href="./about.html">About</a>
+                                <a className="list-group-item" href="./home.html">Home</a> */}
+
+                            {/* 在React中靠路由链接实现切换组件 */}
+                            <Link className="list-group-item" to="/about" >About</Link>
+                            <Link className="list-group-item" to="/home" >Home</Link>
+
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+
+                                {/* 注册路由 */}
+                                <Route path="/about" component={About} />
+                                <Route path="/home" component={Home} />
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
