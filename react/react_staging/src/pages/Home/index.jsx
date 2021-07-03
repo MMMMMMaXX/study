ef21cfc9c7ef7b37bf231fedca29775da3fa2606
MMMMMMaXX <2956619600@qq.com>
2021-07-03 20:24:21 +0800

    '210703'

--- react/react_staging/src/pages/Home/index.jsx
+++ react/react_staging/src/pages/Home/index.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import MyNavLink from '../../components/MyNavLink'
+/* import News from './News'
+import Message from './Message' */
 
-export default class 我是Home的内容 extends Component {
+export default class Home extends Component {
     render() {
         return (
             <div>
                 <h3>我是Home的内容</h3>
+                <ul className="nav nav-tabs">
+                    <li>
+                        <MyNavLink to="/news">News</MyNavLink>
+                    </li>
+                    <li>
+                        <MyNavLink to="/message">Message</MyNavLink>
+                    </li>
+                </ul>
+                {/* 注册路由 */}
+
+                
             </div>
         )
     }
