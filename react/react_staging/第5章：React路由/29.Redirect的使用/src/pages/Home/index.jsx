1b827f2d8f32ecece1c14be3cac55cd233750da0
MMMMMMaXX <2956619600@qq.com>
2021-07-03 17:04:10 +0800

    '210703'

---
+++ react/react_staging/第5章：React路由/29.Redirect的使用/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class 我是Home的内容 extends Component {
+    render() {
+        return (
+            <div>
+                <h3>我是Home的内容</h3>
+            </div>
+        )
+    }
+}
