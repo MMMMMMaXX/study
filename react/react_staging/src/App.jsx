c4ff082b8ff6e2f8503991c4fc39bf1c82d04035
MMMMMMaXX <2956619600@qq.com>
2021-07-04 20:14:21 +0800

    '210704'

--- react/react_staging/src/App.jsx
+++ react/react_staging/src/App.jsx
@@ -1,57 +1,30 @@
-import React, { Component } from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import About from './pages/About'//路由组件
-import Home from './pages/Home'//路由组件
-import Header from './components/Header'//一般组件
-import MyNavLink from './components/MyNavLink'
+import React, { Component } from 'react';
+import { Button, DatePicker, Space } from 'antd';
+import { WechatOutlined, SearchOutlined } from '@ant-design/icons';
+// import 'antd/dist/antd.css';
+import './App.less';
 
 export default class App extends Component {
+    onChange(date, dateString) {
+        console.log(date, dateString);
+    }
     render() {
+        const { RangePicker } = DatePicker;
         return (
             <div>
-                <div className="row">
-                    <div className="col-xs-offset-2 col-xs-8">
-                        <Header />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-2 col-xs-offset-2">
-                        <div className="list-group">
-
-                            {/* 原生html中靠<a>跳转到不同的页面 */}
-                            {/* <a className="list-group-item active" href="./about.html">About</a>
-                                <a className="list-group-item" href="./home.html">Home</a> */}
-
-                            {/* 在React中靠路由链接实现切换组件 */}
-                            {/* <NavLink activeClassName="peiqi" className="list-group-item" to="/about" >About</NavLink>
-                            <NavLink activeClassName="peiqi" className="list-group-item" to="/home" >Home</NavLink> */}
-                            {/* <MyNavLink to="/about" title="About" a={1} b={2} c={3}/> */}
-                            {/* 标签体内容也是一个特殊的标签属性，放在children中 */}
-                            <MyNavLink to="/about" a={1} b={2} c={3}>About</MyNavLink>
-                            <MyNavLink to="/home">Home</MyNavLink>
-                        </div>
-                    </div>
-                    <div className="col-xs-6">
-                        <div className="panel">
-                            <div className="panel-body">
-
-                                {/* 注册路由 */}
-                                <Switch>
-                                    {/* exact严格匹配 */}
-                                    {/* <Route exact path="/about" component={About} />
-                                    <Route exact path="/home" component={Home} /> */}
-
-                                    <Route path="/about" component={About} />
-                                    <Route path="/home" component={Home} />
-
-                                    {/* 重定向 */}
-                                    <Redirect to="/about" />
-                                </Switch>
-
-                            </div>
-                        </div>
-                    </div>
-                </div >
+                App...
+                <button>点击</button>
+                <Button type="primary">按钮1</Button>
+                <Button>按钮2</Button>
+                <Button type="link">按钮3</Button>
+                <WechatOutlined />
+                <Button type="primary" icon={<SearchOutlined />}>
+                    Search
+                </Button>
+                <Space direction="vertical">
+                    <DatePicker onChange={this.onChange} />
+                    <RangePicker showTime />
+                </Space>
             </div >
         )
     }
