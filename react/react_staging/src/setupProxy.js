dddbc5db06e0c335d00bdaebd8eabee8972afaaf
MMMMMMaXX <2956619600@qq.com>
2021-07-08 17:14:24 +0800

    '210708'

--- react/react_staging/src/setupProxy.js
+++ react/react_staging/src/setupProxy.js
@@ -4,6 +4,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function (app) {
     app.use(
+        // @ts-ignore
         proxy('/api1', {    //遇见/api1前缀的请求,就会触发该代理配置
             target: 'http://47.102.202.74:5001',    //请求转发给谁
             changeOrigin: true,                 //控制服务器收到的请求头中Host字段的值,默认为false
