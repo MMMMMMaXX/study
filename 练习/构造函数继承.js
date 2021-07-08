aa6abfce34adacbc026be3f70f02b5bc3353662c
MMMMMMaXX <2956619600@qq.com>
2021-07-08 16:11:21 +0800

    '210708'

--- 练习/构造函数继承.js
+++ 练习/构造函数继承.js
@@ -9,7 +9,8 @@ function Son(name) {
     Father.call(this,name);
 }
 var son = new Son('hhh');
-console.log(son.name);
-console.log(son.height);
+// console.log(son.name);
+// console.log(son.height);
 // 缺点：并不能够继承父类原型上的方法和属性
 // console.log(son.getName());
+
