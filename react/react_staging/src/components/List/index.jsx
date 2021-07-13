95f51e2559985861cc834a7ece0a143b1f8c677a
MMMMMMaXX <2956619600@qq.com>
2021-07-13 22:06:55 +0800

    '210713'

--- react/react_staging/src/components/List/index.jsx
+++ react/react_staging/src/components/List/index.jsx
@@ -35,7 +35,6 @@ export default class List extends Component {
             <div className="row">
                 {
                 isFirst ? <h3>欢迎使用，输入关键字，随后回车或点击搜索</h3> :
-
                 isLoading ? <h3>Loading...</h3> :
                 err ? <h1 style={{ color: 'red' }}>{err}</h1> :
                 users.length === 0 ? <h3>没有找到和关键词相关用户~</h3> :
