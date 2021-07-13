4dbfab71f0e5b15ac636b2848965da29ba325d19
MMMMMMaXX <2956619600@qq.com>
2021-07-13 22:07:15 +0800

    '210713'

--- react/react_staging/src/components/List/index.jsx
+++ react/react_staging/src/components/List/index.jsx
@@ -34,20 +34,20 @@ export default class List extends Component {
         return (
             <div className="row">
                 {
-                isFirst ? <h3>欢迎使用，输入关键字，随后回车或点击搜索</h3> :
-                isLoading ? <h3>Loading...</h3> :
-                err ? <h1 style={{ color: 'red' }}>{err}</h1> :
-                users.length === 0 ? <h3>没有找到和关键词相关用户~</h3> :
-                users.map((userObj) => {
-                    return (
-                        <div key={userObj.id} className="card">
-                            <a rel="noreferrer" href={userObj.html_url} target="_blank">
-                                <img alt="head_portrait" src={userObj.avatar_url} style={{ width: '100px' }} />
-                            </a>
-                            <p className="card-text">{userObj.login}</p>
-                        </div>
-                        )
-                    })
+                    isFirst ? <h3>欢迎使用，输入关键字，随后回车或点击搜索</h3> :
+                        isLoading ? <h3>Loading...</h3> :
+                            err ? <h1 style={{ color: 'red' }}>{err}</h1> :
+                                users.length === 0 ? <h3>没有找到和关键词相关用户~</h3> :
+                                    users.map((userObj) => {
+                                        return (
+                                            <div key={userObj.id} className="card">
+                                                <a rel="noreferrer" href={userObj.html_url} target="_blank">
+                                                    <img alt="head_portrait" src={userObj.avatar_url} style={{ width: '100px' }} />
+                                                </a>
+                                                <p className="card-text">{userObj.login}</p>
+                                            </div>
+                                        )
+                                    })
                 }
             </div>
         )
