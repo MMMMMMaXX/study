e9018b8d352740046326050bc49950b716d16a02
MMMMMMaXX <2956619600@qq.com>
2021-07-19 22:55:57 +0800

    '210719'

--- react/react_staging/src/components/List/index.jsx
+++ react/react_staging/src/components/List/index.jsx
@@ -38,11 +38,11 @@ export default class List extends Component {
                     err ? <h1 style={{ color: 'red' }}>{err}</h1> :
                     users.length === 0 ? <h3>没有找到和关键词相关用户~</h3> :
                     users.map((userObj) => {
-                                        return (
-                                            <div key={userObj.id} className="card">
-                                                <a rel="noreferrer" href={userObj.html_url} target="_blank">
-                                                    <img alt="head_portrait" src={userObj.avatar_url} style={{ width: '100px' }} />
-                                                </a>
+                        return (
+                            <div key={userObj.id} className="card">
+                                <a rel="noreferrer" href={userObj.html_url} target="_blank">
+                                    <img alt="head_portrait" src={userObj.avatar_url} style={{ width: '100px' }} />
+                                </a>
                                                 <p className="card-text">{userObj.login}</p>
                                             </div>
                                         )
