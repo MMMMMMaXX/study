849702beb366bbb25243eaaac90ec0c56648e05e
manxin <manxin@xiaomi.com>
2021-07-22 16:53:00 +0800

    210722

---
+++ 刷题/2. 两数相加.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var res = new ListNode(0);
+    let resNode = res;
+    var temp = 0;
+    var ifUp = 0;
+    while(l1 || l2) {
+        if(l1) {
+            temp += l1.val;
+            l1 = l1.next;
+        }
+        if(l2) {
+            temp += l2.val;
+            l2 = l2.next;
+        }
+        if(ifUp === 1) {
+            temp ++;
+            ifUp = 0;
+        }
+        if(temp > 9) {
+            temp = temp - 10;
+            ifUp = 1;
+        }
+        res.next = new ListNode(temp);
+        res = res.next;
+        temp = 0;
+    }
+    if(ifUp === 1) {
+        res.next = new ListNode(1);
+    }
+    return resNode.next;
+};
+console.log(addTwoNumbers([4],[6]));
+
+/**
+ * 链表问题
+ */
