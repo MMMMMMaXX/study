d34f4194a0bd945585a4bf50b9e2bcb20e83f6b4
manxin <manxin@xiaomi.com>
2021-07-22 17:20:38 +0800

    210722

---
+++ 刷题/3. 无重复字符的最长子串.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    var res = 0;
+    var temp = [];
+    for(var i = 0; i < s.length; i ++) {
+        if(temp.indexOf(s[i]) === -1) {
+            temp.push(s[i]);
+            // console.log(temp);
+        } else {
+            temp.shift();
+            // console.log(temp);
+            i--;
+            continue;
+        }
+        res = Math.max(res, temp.length);
+    }
+    return res;
+};
+console.log(lengthOfLongestSubstring('qwertqqq123'));
