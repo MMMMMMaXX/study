495c12fff2279e296eda8e137c7b71e634348f5f
manxin <manxin@xiaomi.com>
2021-08-02 10:35:50 +0800

    210802

--- 刷题/数组/26. 删除有序数组中的重复项.js
+++ 刷题/数组/26. 删除有序数组中的重复项.js
@@ -3,24 +3,14 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let doNum = undefined;
-    let doLength = 1;
-    let doStart = 0;
-    for (let i = 0; i < nums.length; i ++) {
-        if(!doNum) {
-            doNum = nums[i];
-            doStart = i;
-        } else if(nums[i] === doNum) {
-            doLength ++;
-        } else {
-            console.log(doNum,doLength,doStart);
-            nums.splice(doStart,doLength,doNum);
-            i = i - doLength + 1;
-            doNum = undefined;
-            doLength = 1;
-            doStart = 0;
+    let R = 0 ,L = 0
+    while(R < nums.length){
+        if(nums[R] != nums[L]){
+            L++
+            nums[L] = nums[R]
         }
+        R++
     }
-    return nums;
+    return L+1
 };
-console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,0,1,1,1,1,1,1,2,2,2,2]));
