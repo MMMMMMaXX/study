c65d7d6d325fa75cd60af78171566f2fce76f477
MMMMMMaXX <2956619600@qq.com>
2021-08-04 22:57:44 +0800

    '210804'

--- 天梯赛/练习集/L1-006 连续因子 (20 分).cpp
+++ 天梯赛/练习集/L1-006 连续因子 (20 分).cpp
@@ -25,7 +25,6 @@ int main()
                         max = count;
                         begain = a;
                         end = j + 1;
-                        //	printf("begain:%d,end:%d,count=%d,max=%d\n",begain,end,count,max);
                     }
                     count = 0;
                     a = -1;
