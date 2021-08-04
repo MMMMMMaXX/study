b2a1727fb0f897c11621b2ad6a24ec75353b48e2
MMMMMMaXX <2956619600@qq.com>
2021-08-04 22:51:45 +0800

    '210804'

--- 天梯赛/basic level/1032 挖掘机技术哪家强 (20 分).cpp
+++ 天梯赛/basic level/1032 挖掘机技术哪家强 (20 分).cpp
@@ -1,23 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int num,sz[100001]={0},i,j,result,max,score,hao;
-	scanf("%d",&num);
-	for(i=0;i<num;i++)
-	{
-		scanf("%d %d",&hao,&score);
-		sz[hao]=sz[hao]+score;
-	}
-	max=sz[0];
-	for(j=1;j<=num;j++)
-	{
-		if(sz[j]>max)
-		{
-			max=sz[j];
-			result=j;
-		}
-	}
-	printf("%d %d",result,sz[result]);
-	return 0;
+    int num, sz[100001] = {0}, i, j, result, max, score, hao;
+    scanf("%d", &num);
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d %d", &hao, &score);
+        sz[hao] = sz[hao] + score;
+    }
+    max = sz[0];
+    for (j = 1; j <= num; j++)
+    {
+        if (sz[j] > max)
+        {
+            max = sz[j];
+            result = j;
+        }
+    }
+    printf("%d %d", result, sz[result]);
+    return 0;
 }
