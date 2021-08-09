97c4e2376dce659797ca24813ab71fb757b7ce1a
MMMMMMaXX <2956619600@qq.com>
2021-08-09 23:41:41 +0800

    '210809'

--- leetcode/链表/2. 两数相加.js
+++ leetcode/链表/2. 两数相加.js
@@ -11,29 +11,29 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- function ListNode(val, next) {
-     this.val = (val===undefined ? 0 : val)
-     this.next = (next===undefined ? null : next)
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
 }
-var addTwoNumbers = function(l1, l2) {
+var addTwoNumbers = function (l1, l2) {
     var res = new ListNode(0);
     let resNode = res;
     var temp = 0;
     var ifUp = 0;
-    while(l1 || l2) {
-        if(l1) {
+    while (l1 || l2) {
+        if (l1) {
             temp += l1.val;
             l1 = l1.next;
         }
-        if(l2) {
+        if (l2) {
             temp += l2.val;
             l2 = l2.next;
         }
-        if(ifUp === 1) {
-            temp ++;
+        if (ifUp === 1) {
+            temp++;
             ifUp = 0;
         }
-        if(temp > 9) {
+        if (temp > 9) {
             temp = temp - 10;
             ifUp = 1;
         }
@@ -41,12 +41,12 @@ var addTwoNumbers = function(l1, l2) {
         res = res.next;
         temp = 0;
     }
-    if(ifUp === 1) {
+    if (ifUp === 1) {
         res.next = new ListNode(1);
     }
     return resNode.next;
 };
-console.log(addTwoNumbers([4],[6]));
+console.log(addTwoNumbers([4], [6]));
 
 /**
  * 链表问题
