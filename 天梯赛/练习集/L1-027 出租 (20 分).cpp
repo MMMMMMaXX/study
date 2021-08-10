6f326c8f170677c09d9937fb92c49ae7bcb939ec
MMMMMMaXX <2956619600@qq.com>
2021-08-10 23:44:56 +0800

    '210810'

--- 天梯赛/练习集/L1-027 出租 (20 分).cpp
+++ 天梯赛/练习集/L1-027 出租 (20 分).cpp
@@ -2,15 +2,7 @@
 using namespace std;
 int main()
 {
-    char shuz[12];
-    int sz[10], flage0 = 0, n = 0, sz0[10], flage1 = 0, i, j, k, l;
-    for (i = 0; i < 10; i++)
-        sz[i] = i;
-    cin >> shuz;
-    for (i = 0; shuz[i] != '\0'; i++)
-    {
-        sz[shuz[i] - '0'] = -1;
-    }
+    
     printf("int[] arr = new int[]{");
     for (j = 9; j >= 0; j--)
     {
