374158f26f25527f8ae8513d2078c09b76c2fe44
Max <2956619600@qq.com>
2021-08-11 17:49:10 +0800

    tx面试题练习

--- 练习/16.tx面试题.js
+++ 练习/16.tx面试题.js
@@ -1,8 +1,13 @@
 async function test(){
-    const p = new Promise(r => setTimeout(r, 1000))
-    setTimeout(console.log('a'), 0)
-    await p
-    console.log('b');
-  }
-  test() 
-  
+  const p = new Promise(r => setTimeout(r, 1000))
+  setTimeout(() => console.log('a'), 0)
+  await p
+  console.log('b');
+}
+test() 
+/**
+a，b谁先执行
+Promise {<pending>}
+a
+b
+ */
