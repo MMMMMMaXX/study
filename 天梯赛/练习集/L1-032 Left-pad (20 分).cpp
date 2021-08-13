91390429c6dd186cafa1dd61ca21c163f0382e0d
Max <2956619600@qq.com>
2021-08-13 19:23:43 +0800

    210813

--- 天梯赛/练习集/L1-032 Left-pad (20 分).cpp
+++ 天梯赛/练习集/L1-032 Left-pad (20 分).cpp
@@ -2,6 +2,35 @@
 using namespace std;
 int main()
 {
+	int num,i,n=0;
+	char zf;
+	string a;
+	scanf("%d %c",&num,&zf);
+	getchar();
+	getline(cin,a);
+	int str=a.length();
+//	int str=strlen(a);
+	if(str>num)
+	{
+		for(i=str-num;i<str;i++)
+			printf("%c",a[i]);
+	}
+	else if(str==num)
+		cout<<a;
+	else if(str<num)
+	{
+		for(i=0;i<num;i++)
+		{
+			if(i<num-str)
+			{
+				printf("%c",zf);
+				n++;
+			}
+				
+			else
+				printf("%c",a[i-n]);
+		}	
+	}
 	
 	return 0;
 }
