e9287e2a8b0fae394b2f177add4450da43b16cce
MMMMMMaXX <2956619600@qq.com>
2021-08-22 23:00:44 +0800

    '210822'

--- leetcode/5834. 使用特殊打字机键入单词的最少时间 显示英文描述 .js
+++ leetcode/5834. 使用特殊打字机键入单词的最少时间 显示英文描述 .js
@@ -2,13 +2,13 @@
  * @param {string} word
  * @return {number}
  */
-var minTimeToType = function(word) {
+var minTimeToType = function (word) {
     let now = 'a';
     let sum = 0;
     for (let i = 0; i < word.length; i++) {
-        if(now.charCodeAt() < 'n'.charCodeAt()){
-            if (word[i] <= String.fromCharCode(now.charCodeAt()+13)) {
-                if(word[i].charCodeAt() - now.charCodeAt() > 0) {
+        if (now.charCodeAt() < 'n'.charCodeAt()) {
+            if (word[i] <= String.fromCharCode(now.charCodeAt() + 13)) {
+                if (word[i].charCodeAt() - now.charCodeAt() > 0) {
                     sum = sum + word[i].charCodeAt() - now.charCodeAt() + 1;
                     // console.log('you',word[i].charCodeAt() - now.charCodeAt() + 1);
                 } else {
@@ -18,10 +18,10 @@ var minTimeToType = function(word) {
             } else {
                 sum = sum + 26 - (word[i].charCodeAt() - now.charCodeAt()) + 1;
                 // console.log('zuo',26 - (word[i].charCodeAt() - now.charCodeAt()) + 1);
-            }    
+            }
         } else {
             if (word[i] <= String.fromCharCode(now.charCodeAt())) {
-                if(now.charCodeAt() - word[i].charCodeAt() > 13) {
+                if (now.charCodeAt() - word[i].charCodeAt() > 13) {
                     sum = sum + 26 - (now.charCodeAt() - word[i].charCodeAt()) + 1;
                     // console.log('you2',26 - (now.charCodeAt() - word[i].charCodeAt()) + 1);
                 } else {
@@ -29,7 +29,7 @@ var minTimeToType = function(word) {
                     // console.log('you2',now.charCodeAt() - word[i].charCodeAt() + 1);
                 }
             } else {
-                if(word[i].charCodeAt() - now.charCodeAt() > 13) {
+                if (word[i].charCodeAt() - now.charCodeAt() > 13) {
                     sum = sum + (word[i].charCodeAt() - now.charCodeAt()) + 1;
                     // console.log('zuo2',(word[i].charCodeAt() - now.charCodeAt()) + 1);
                 } else {
