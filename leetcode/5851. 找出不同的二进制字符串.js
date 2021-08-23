1bdc812dabae507555bcf5c66663a1d0ab6db48e
MMMMMMaXX <2956619600@qq.com>
2021-08-23 22:22:52 +0800

    '210823'

--- leetcode/5851. 找出不同的二进制字符串.js
+++ leetcode/5851. 找出不同的二进制字符串.js
@@ -2,11 +2,11 @@
  * @param {string[]} nums
  * @return {string}
  */
-var findDifferentBinaryString = function(nums) {
+var findDifferentBinaryString = function (nums) {
     var t = {};
     var nowN;
     var max = -1;
-    for(let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         t[nums[i]] = 1;
         // if(nums[i].charCodeAt().toString(2))
     }
@@ -15,21 +15,21 @@ var findDifferentBinaryString = function(nums) {
     var nowNL;
     // console.log('strL',strL);
     // console.log('pow',Math.pow(2,strL));
-    for(let i = 0; i < Math.pow(2,strL); i++) {
-        nowN=i.toString(2);
+    for (let i = 0; i < Math.pow(2, strL); i++) {
+        nowN = i.toString(2);
         // console.log('nowN',nowN);
         nowNL = nowN.length
-        if(nowNL<strL) {
-            for(let j=0;j<strL-nowNL;j++){
-                nowN = "0"+nowN;
+        if (nowNL < strL) {
+            for (let j = 0; j < strL - nowNL; j++) {
+                nowN = "0" + nowN;
                 // console.log('do',strL-nowNL);
             }
             // console.log('n',nowN);
         }
-        if(t[nowN]!==1){
+        if (t[nowN] !== 1) {
             return nowN;
         }
     }
 };
-console.log(findDifferentBinaryString(["111","011","001"]));
+console.log(findDifferentBinaryString(["111", "011", "001"]));
 // console.log("11".charCodeAt());
