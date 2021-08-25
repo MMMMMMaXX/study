3fb4841727d7982e5624c21a3d68cc49c3702a02
MMMMMMaXX <2956619600@qq.com>
2021-08-25 23:43:27 +0800

    '210825'

--- 练习/3.js
+++ 练习/3.js
@@ -1,5 +1,20 @@
-var arr = [5,5,6,2,1,3];
-var result = arr.reduce((pre,value,index) => {
+var arr = [5, 5, 6, 2, 1, 3];
+var result = arr.reduce((pre, value, index) => {
     return pre + value;
-},0)
-console.log(result); 
+}, 0)
+console.log(result);
+
+
+function P() {
+    this.a = [];
+}
+P.prototype.b = [];
+let child1 = new P();
+child1.a.push(1);
+child1.b.push(2);
+console.log(child1.a);
+console.log(child1.b);
+
+let child2 = new P();
+console.log(child2.a);
+console.log(child2.b);
