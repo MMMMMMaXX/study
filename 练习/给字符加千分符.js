5b8217efded468f938646123a8d5de82fb8cdee2
MMMMMMaXX <2956619600@qq.com>
2021-09-05 20:38:28 +0800

    210905

--- 练习/给字符加千分符.js
+++ 练习/给字符加千分符.js
@@ -1,5 +1,19 @@
-function test( str ) {
-    // write code here
+// function test( str ) {
+//     // write code here
+//     var res = [];
+//     var numTime = 1;
+//     for(let i = str.length - 1; i >= 0; i--) {
+//         res.push(str[i]);
+//         if(numTime % 3 === 0 && i !== 0) {
+//             res.push(',');
+//         }
+//         numTime++;
+//     }
+//     return res.reverse().join('');
+// }
+// console.log(test("123456789"));
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+function f(str) {
     var res = [];
     var numTime = 1;
     for(let i = str.length - 1; i >= 0; i--) {
@@ -11,4 +25,5 @@ function test( str ) {
     }
     return res.reverse().join('');
 }
-console.log(test("123456789"));
+
+console.log(f('10000000000'));
