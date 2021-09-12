3224308829ba8557df98993c0893fc810512d356
MMMMMMaXX <2956619600@qq.com>
2021-09-12 16:42:17 +0800

    210912

---
+++ 练习/errtgdf.js
@@ -0,0 +1,24 @@
+function fn(){
+    let inn = '';
+    let s,e; 
+    inn = 'IBIPBPIPPPP';
+    s=0;
+    e=10;
+    let res = [];
+    for(let i=s;i<s+e;i++){
+        if(inn[i]==='I'&&res.length===0){
+            res.push(inn[i]);
+        } else if(inn[i]==='I'&&res.length>0) {
+            console.log(res.join(''))
+            res=[];
+            res.push(inn[i]);
+        } else if(inn[i]!=='I'){
+            res.push(inn[i]);
+        }
+    }    
+    if(res.length>1){
+        console.log(res.join(''));
+    }
+}
+fn()
+
