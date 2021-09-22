9d97b721ca7a20c1ee45407125b260550456e623
MMMMMMaXX <2956619600@qq.com>
2021-09-22 16:50:12 +0800

    210922

--- leetcode/5876. 数组美丽值求和.js
+++ leetcode/5876. 数组美丽值求和.js
@@ -27,12 +27,9 @@
             sum+=2;
             max=nums[i];
         } else{
-
             if(nums[i]>nums[i-1]&&nums[i]<nums[i+1]){
     // console.log('11111');
     // console.log(nums[i]);
-
-
                 sum+=1;
                 max=nums[i];
             }
